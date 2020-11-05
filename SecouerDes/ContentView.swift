11e68cfcef3c99b90dff28c7b5b0d3f44cb23dbe
Philippe MICHEL <philippe.michel1@gmail.com>
2020-11-05 15:38:49 +0100

    Initial Commit

---
+++ SecouerDes/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SecouerDes
+//
+//  Created by Philippe MICHEL on 05/11/2020.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
